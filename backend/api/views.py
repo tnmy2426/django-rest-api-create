ecb1394743135c52bcfdfcdcf3b6d6b256d22503
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-11-17 02:37:51 +0600

    ViewSet created in views.py using ModelViewSet

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import MovieSerializer
+from .models import Movie
+
+# For ViewSet
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 # Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()

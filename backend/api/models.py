d4259bfd579de24c202518a061110248a03bc1be
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-11-17 02:22:42 +0600

    Serializer created using ModelSerializer [ Rest-Framework ]

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,3 +6,6 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.title

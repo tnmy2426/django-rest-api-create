d21556406bbe422b70051e79259c5e35aedbf1fc
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-11-17 02:06:40 +0600

    Model / Database Created for Movie

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])

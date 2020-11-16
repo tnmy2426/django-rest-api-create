585ebb54b59286d2c3d627b358947e02e58f9fd5
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-11-17 02:45:31 +0600

    Add Routers API in urls.py and getting api-root

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import MovieViewSet
 
-urlpatterns = [
-    # path('', include('api.urls')),
-]
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('movie', MovieViewSet)
+
+urlpatterns = router.urls
